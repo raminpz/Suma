76fd5fb92c69e30706061226fad72333511b8910
raminpz <raminp.1012@gmail.com>
2022-07-28 15:41:04 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Suma de tres numero = " + suma(10,10,10));
+
+    }
+
+    public static  int suma(int num1, int num2, int num3){
+        return num1 + num2 + num3;
+    }
+}
