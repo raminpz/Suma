d9da549eefd1834f17b02ad7e855dfa3cfd0451a
raminpz <raminp.1012@gmail.com>
2022-07-28 15:45:24 -0500

    Completo

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,15 @@ public class Main {
 
 
         System.out.println("Suma de tres numero = " + suma(10,10,10));
+        System.out.println("Coche de  = " + coche(3) + " puertas");
 
     }
 
     public static  int suma(int num1, int num2, int num3){
         return num1 + num2 + num3;
+    }
+
+    public  static int coche (int numPuertas){
+        return numPuertas + 1;
     }
 }
